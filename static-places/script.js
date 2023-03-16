f912a3de08f17216d9ec378ab2c9eb3d9b635330
Vu Ba Huan <vbhuan@cmcglobal.vn>
2023-03-16 17:44:48 +0700

    Update position model

--- static-places/script.js
+++ static-places/script.js
@@ -25,21 +25,24 @@ function staticLoadPlaces() {
 var models = [
     {
          url: './assets/magnemite/scene.gltf',
-        scale: '0.01 0.01 0.01',
+        scale: '0.02 0.02 0.02',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 45 0',
+        position: '0 1.6 0'
     },
     {
          url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
+        position: '0 1.6 0'
     },
     {
          url: './assets/dragonite/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
+        position: '0 1.6 0'
     },
 
 ];
